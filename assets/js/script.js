3c03c40a6a70ea9d14efee3243e65d085244bfca
Chelsea <chelsea.chapman4@hotmail.com>
2023-11-17 19:16:09 +0000

    adds js and css files as well as addin pg pseudocode to the js file

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// Search for a city and return the weather data for that city
+// log the search history on the page
+// show the city name
+// show the date
+// show icons of weather conditions
+// show the temperature
+// show the humidity
+// show the wind speed
+
+// show the future weather for the next 5 days
+// show the next 5 dates
+// show the weather conditions with an icon for the next 5 days
+// show the temperature for the next 5 days
+// show the humidity for the next 5 days
+
+// Click a previous search and the weather conditions will show
